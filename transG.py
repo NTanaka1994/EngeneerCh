e5d261468121e4bc17cb2ef48534a091e505cb41
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 21:19:24 +0900

    Update transG.py

--- transG.py
+++ transG.py
@@ -1,26 +1,26 @@
 from googletrans import Translator
 import pandas as pd
 
-df=pd.read_csv("trans.csv",encoding="shift-jis")
-name=df["name"].values
-sent=df["sentence"].values
+df = pd.read_csv("trans.csv", encoding="shift-jis")
+name = df["name"].values
+sent = df["sentence"].values
 
-trans=Translator(service_urls=['translate.googleapis.com'])
+trans = Translator(service_urls=['translate.googleapis.com'])
 
-tmp=[]
-total=[]
+tmp = []
+total = []
 for i in range(len(sent)):
     while True:
         try:
-            transed=trans.translate(sent[i],dest="ja")
+            transed=trans.translate(sent[i], dest="ja")
             break
         except Exception as e:
-            trans=Translator(service_urls=['translate.googleapis.com'])
+            trans = Translator(service_urls=['translate.googleapis.com'])
     tmp.append(name[i])
     tmp.append(sent[i])
     tmp.append(transed.text)
     total.append(tmp)
     tmp=[]
 dft=pd.DataFrame(total)
-dft.columns=["name","English","日本語"]
-dft.to_csv("trans_result.csv",encoding="shift-jis",index=False)
+dft.columns=["name", "English", "日本語"]
+dft.to_csv("trans_result.csv", encoding="shift-jis", index=False)
