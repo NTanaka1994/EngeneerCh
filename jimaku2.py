3d645bda4cf99d43e997299424025937a227ee34
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 21:14:49 +0900

    Update jimaku2.py

--- jimaku2.py
+++ jimaku2.py
@@ -7,70 +7,70 @@ import numpy as np
 
 #mp4からwavファイルに変換
 for path in glob.glob("Movie/*.mp4"):
-    cmd=r'ffmpeg -i Movie/'+path[6:]+" Movie/"+path[6:]+".wav"
+    cmd = r'ffmpeg -i Movie/' + path[6:] + " Movie/" + path[6:] + ".wav"
     subprocess.call(cmd)
 
 #文字起こし行う
-total=[]
-r=sr.Recognizer()
+total = []
+r = sr.Recognizer()
 for path in glob.glob("Movie/*.wav"):
     with sr.AudioFile("Movie/"+path[6:]) as src:
-        ad=r.record(src)
-    text=r.recognize_google(ad, language='ja-JP')
+        ad = r.record(src)
+    text = r.recognize_google(ad, language='ja-JP')
     total.append(text)
 
 #台本の読み込み
-f=open("sinario/sample.txt","r",encoding="utf-8")
-s=f.read()
+f = open("sinario/sample.txt", "r", encoding="utf-8")
+s = f.read()
 f.close()
-rm=s.split("\n")
+rm = s.split("\n")
 
 #形態素解析
-t=Tokenizer()
-train=[]
-test=[]
-sumt=[]
+t = Tokenizer()
+train = []
+test = []
+sumt = []
 for i in range(len(total)):
-    train.append(t.tokenize(total[i],wakati=True))
-    test.append(t.tokenize(rm[i],wakati=True))
+    train.append(t.tokenize(total[i], wakati=True))
+    test.append(t.tokenize(rm[i], wakati=True))
 for i in range(len(train)):
     sumt.append(train[i])
 for i in range(len(test)):
     sumt.append(test[i])
 
 #分かち書き
-x_doc=[]
+x_doc = []
 for i in range(len(sumt)):
-    doc=""
+    doc = ""
     for j in range(len(sumt[i])):
-        doc=doc+" "+sumt[i][j]
+        doc = doc + " " + sumt[i][j]
     x_doc.append(doc)
 
 #TF-IDF
-tivec=tfidf()
-x=tivec.fit_transform(x_doc)
-x=x.toarray()
+tivec = tfidf()
+x = tivec.fit_transform(x_doc)
+x = x.toarray()
 
 #認識された文書と脚本を分割
-x_train=x[0:int(len(x)/2)]
-x_test=x[int(len(x)/2):len(x)]
+x_train = x[0:int(len(x)/2)]
+x_test = x[int(len(x)/2):len(x)]
 
-out=[]
+out = []
 #脚本と入れ替え
 for i in range(len(x_train)):
-    num=0
-    sim=np.dot(x_train[i],x_test[0]) / (np.linalg.norm(x_train[i]) * np.linalg.norm(x_test[0]))
+    num = 0
+    sim = np.dot(x_train[i], x_test[0]) / (np.linalg.norm(x_train[i])*np.linalg.norm(x_test[0]))
     for j in range(len(x_test)):
-        sim2=np.dot(x_train[i],x_test[j]) / (np.linalg.norm(x_train[i]) * np.linalg.norm(x_test[j]))
-        if sim2>sim:
-            num=j
-            sim=sim2
+        sim2 = np.dot(x_train[i], x_test[j]) / (np.linalg.norm(x_train[i]) * np.linalg.norm(x_test[j]))
+        if sim2 > sim:
+            num = j
+            sim = sim2
     out.append(rm[num])
 
 #修正した文書を保存
-i=0
+i = 0
 for path in glob.glob("Movie/*.wav"):
-    f=open("Movie/"+path[6:]+".txt","w",encoding="shift-jis")
+    f = open("Movie/"+path[6:]+".txt", "w", encoding="shift-jis")
     f.write(out[i])
     f.close()
-    i=i+1
+    i = i + 1
